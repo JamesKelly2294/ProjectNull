2b8301a7d9b7fa529f04c8031329fd5a4108a16d
James Kelly <dxk2294@rit.edu>
2019-10-05 00:22:14 -0700

    Proto factory made

---
+++ ProjectNull/Assets/CameraFixFuckupScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFixFuckupScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponentInChildren<Camera>().farClipPlane = 4000;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ProjectNull/Assets/EnableCanvasOnLaunch.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableCanvasOnLaunch : MonoBehaviour
+{
+    public GameObject canvas;
+    public GameObject eventSystem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas.SetActive(true);
+        eventSystem.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
