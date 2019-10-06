c7e2f269198b8f3fff337e0ae9658ef4b0d5e5cc
Jacob Ingalls <jacobingalls@yahoo.com>
2019-10-05 18:30:52 -0700

    Move conveyer logic to the rollers to allow for turns

---
+++ ProjectNull/Assets/Scripts/ConveyorRoller.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorRoller : MonoBehaviour
+{
+
+    public GameObject roller;
+
+    public List<GameObject> currentObjects;
+
+    public float speed = 0.25f;
+
+    public float rollerSpeed = 180f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        roller.transform.localEulerAngles += new Vector3(0, -rollerSpeed * Time.deltaTime, 0);
+
+        var dir = transform.TransformVector(new Vector3(speed * Time.deltaTime, 0, 0));
+        foreach (var obj in currentObjects) {
+            obj.transform.Translate(obj.transform.InverseTransformVector(dir));
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<Box>() != null) {
+            currentObjects.Add(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        currentObjects.Remove(other.gameObject);
+    }
+}
